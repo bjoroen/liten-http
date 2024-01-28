0159f06fb07b79b54078a0af37931367c354d670
eirik <bjoroen.eirik@gmail.com>
2024-01-28 19:16:19 +0100

    Added method for adding content, and spesific method for html and json

--- src/response.rs
+++ src/response.rs
@@ -43,14 +43,36 @@ impl Response {
         Self::default()
     }
 
-    pub fn status(self, status: Status) -> Response {
+    pub fn status(self, status: Status) -> Self {
         Response { status, ..self }
     }
 
-    pub fn header(self, h: Header) -> Response {
+    pub fn header(self, h: Header) -> Self {
         let mut header = self.header;
         header.push(h);
         Response { header, ..self }
+    }
+
+    pub fn content(self, content: &str, content_type: &str) -> Response {
+        let content_length_header = Header::new("Content-Length", &content.len().to_string());
+        let content_type_header = Header::new("Content-Type", content_type);
+        let mut header = vec![];
+        header.push(content_length_header);
+        header.push(content_type_header);
+
+        Self {
+            header,
+            body: Some(content.to_string()),
+            ..self
+        }
+    }
+
+    pub fn html(self, content: &str) -> Self {
+        Self::content(self, content, "text/html")
+    }
+
+    pub fn json(self, content: &str) -> Self {
+        Self::content(self, content, "application/json")
     }
 }
 
@@ -74,6 +96,30 @@ mod test {
         assert_eq!(
             response.to_string(),
             "HTTP/1.1 200 OK\r\nContent-Length: 69\r\n\r\n"
+        )
+    }
+
+    #[test]
+    fn response_with_html_body() {
+        let response = Response::new()
+            .status(Status::Ok)
+            .html("<HTML><h1>Hello World</h1></HTML>");
+
+        assert_eq!(
+            response.to_string(),
+            "HTTP/1.1 200 OK\r\nContent-Length: 33\r\nContent-Type: text/html\r\n\r\n<HTML><h1>Hello World</h1></HTML>"
+        )
+    }
+
+    #[test]
+    fn response_with_json_body() {
+        let response = Response::new()
+            .status(Status::Ok)
+            .json("{\"hello\": \"world\"}");
+
+        assert_eq!(
+            response.to_string(),
+            "HTTP/1.1 200 OK\r\nContent-Length: 18\r\nContent-Type: application/json\r\n\r\n{\"hello\": \"world\"}"
         )
     }
 }
